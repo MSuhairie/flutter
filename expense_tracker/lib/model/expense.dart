37345086051cf5055efcaed54fb400d83878d4ed
msuhairie <msuhairie255@gmail.com>
2024-03-24 01:41:40 +0700

    update

---
+++ expense_tracker/lib/model/expense.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
+
+const uuid = Uuid();
+
+enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
+
+class Expense {
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.category,
+    required this.date,
+  }) : id = uuid.v4();
+
+  final String id;
+  final String title;
+  final DateTime date;
+  final double amount;
+  final Category category;
+
+  String get formattedDate {
+    return formatter.format(date); 
+  }
+}
